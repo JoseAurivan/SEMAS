1cb0b590aa7a61130ab13e2864b6a5194d2a59cd
joseaurivan <aurivanfilho2013@gmail.com>
2021-11-19 11:18:19 -0300

    Acabando com duplicatas no service

--- Domain/Models/PessoaEndereco.cs
+++ Domain/Models/PessoaEndereco.cs
@@ -1,10 +1,34 @@
-﻿namespace Domain.Models
+﻿using System;
+
+namespace Domain.Models
 {
-    public class PessoaEndereco
+    public class PessoaEndereco:IEquatable<PessoaEndereco>
     {
         public Pessoa Pessoa { get; set; }
         public int PessoaId { get; set; }
         public Endereco Endereco { get; set; }
         public int EnderecoId { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is PessoaEndereco pessoaEndereco) return Equals(pessoaEndereco);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PessoaId,EnderecoId);
+        }
+
+        public bool Equals(PessoaEndereco other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (GetHashCode() != other.GetHashCode()) return false;
+            
+            return PessoaId == other.PessoaId
+                   && Equals(Endereco, other.Endereco) 
+                   && EnderecoId == other.EnderecoId;
+        }
     }
 }
